0fd6baf807138a819b1801ce3870345b33715f90
Gabrielbemo <gabrielbemoura@gmail.com>
2025-01-19 17:54:32 -0300

    adding BP and animation idleRun to aura and enemies

--- Source/Aura/Private/Character/AuraCharacterBase.cpp
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -7,6 +7,9 @@ AAuraCharacterBase::AAuraCharacterBase()
 {
 	PrimaryActorTick.bCanEverTick = false;
 
+	Wapon = CreateDefaultSubobject<USkeletalMeshComponent>("Wapon");
+	Wapon->SetupAttachment(GetMesh(), FName(TEXT("WaponHandSocket")));
+	Wapon->SetCollisionEnabled(ECollisionEnabled::NoCollision);
 }
 
 void AAuraCharacterBase::BeginPlay()
